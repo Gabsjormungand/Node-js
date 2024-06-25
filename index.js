b360458fa0261f4224c3ab592b87a5598fd1486d
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-06-24 22:23:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get("/heroes", function(req, res){
     res.send(herois)
 })
 
+
+//EnsPoint listagem de um herói (READ SINGLE => [GET])
 app.get("/heroes/:id", function(req, res){
     //acessar o parametro de rota ID
     const id = req.params.id
@@ -17,6 +19,14 @@ app.get("/heroes/:id", function(req, res){
 
     //eu envio o item encontrado como resposta
     res.send(umheroi)
+})
+
+//todo o corpo da requisição virá como JSON
+app.use(express.json())
+
+//EndPoint de criação (CREAT => [POST])
+app.post("/heroes", function(req, res){
+    res.send(req.body)
 })
 
 app.listen(3000)
